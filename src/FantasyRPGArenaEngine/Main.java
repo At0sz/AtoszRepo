ad009fd26a5fae16f4428c16fa3bbcf8e82bbda4
At0sz <atoszfilms@gmail.com>
2026-04-05 18:55:21 +0200

    Fantasy RPG arena engine Main testing

--- src/FantasyRPGArenaEngine/Main.java
+++ src/FantasyRPGArenaEngine/Main.java
@@ -13,15 +13,24 @@ public class Main {
         arena.addHero(assassinOne);
         arena.addHero(assassinTwo);
 
-        paladinOne.basicAttack(assassinOne); assassinOne.castUltimate(paladinOne);
+        paladinOne.basicAttack(assassinOne);
+        assassinOne.castUltimate(paladinOne);
 
         System.out.println("All the heroes: ");
         arena.getAliveHeroes().forEach(System.out::println);
-        System.out.println("Heroes by role: ");
+        System.out.println("\nHeroes by role: ");
         arena.groupHeroesByRole().forEach((role, heroes) -> {
             System.out.println(role + " heroes: " + heroes);
         });
         System.out.println("\nHero with most health: ");
         System.out.println(arena.getHealthiestAliveHero());
+
+        paladinOne.takeDamage(170);
+        assassinOne.castUltimate(paladinOne);
+        System.out.println("\nPaladin hp: " + paladinOne.getHp());
+        System.out.println("\nPaladin get killed");
+
+        System.out.println("\nAll the heroes: ");
+        arena.getAliveHeroes().forEach(System.out::println);
     }
 }
